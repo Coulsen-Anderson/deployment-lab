b2727b6b198aa9d4ea4e528065604cb2dc7cbdfe
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-09-21 16:16:06 -0600

    forgot css in indes.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+})
+
 
 app.listen(port, () => {
     console.log('Docked at port ' + port)
