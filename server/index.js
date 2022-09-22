a9afc02054fdf909c44adb1cbf735b07f4685102
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-09-22 15:22:54 -0600

    rollbar

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
 const path = require('path')
+const cors = require('cors')
+
 
 require('dotenv').config()
 
@@ -9,18 +10,28 @@ const port =  process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '74666d63d8d2410b956608bcb788a07d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
-
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/styles.css'))
 })
 app.get('pexels-wallace-chuck-3587477.jpg', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/pexels-wallace-chuck-3587477.jpg'))
 })
+
 
 
 app.listen(port, () => {
